992c249b9bc17c425508227cb496105f56fa1af8
Max Huang <max@thecompany-studio.com>
2024-10-02 00:37:05 +0800

    Refactor code struture

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', name: 'Home', component: () => import('@/views/HomeView.vue') },
+    { path: '/woodtotem/:id', name: 'Woodtotem', component: () => import('@/views/WoodtotemView.vue') }
+  ]
+})
+
+createApp(App)
+  .use(router)
+  .mount('#app')
